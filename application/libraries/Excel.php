d5197a7afe3cc589cf1de442f5fce748d756ac25
cherra <jorge@matica.mx>
2014-03-07 09:51:59 -0600

    Librería PHPExcel

---
+++ application/libraries/Excel.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Description of excel
+ *
+ * @author cherra
+ */
+
+require_once 'excel/PHPExcel.php';
+//require_once 'excel/PHPExcel/IOFactory.php';
+
+class Excel {
+    
+    public function __construct() {
+        parent::__construct();
+    }
+}
+
+?>
